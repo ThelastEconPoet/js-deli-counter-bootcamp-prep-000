7ee57d83083fc2dadbc2dd0375bfcdcd1711f549
Aaron G Meininger <a.g.meininger@gmail.com>
2018-05-17 18:21:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,12 +25,3 @@ function currentLine(katzDeliLine) {
   return (currentLine())
 }
 
-function nowServing(x) {
-  if (x.length === 0) {
-    return "There is nobody waiting to be served!"
-  } else {
-    var name = x[0];
-    x.splice(0, 1);
-    return "Currently serving " + name + ".";
-  }
-}
